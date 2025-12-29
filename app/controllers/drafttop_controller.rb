ab65332c663b097cd59a9497c43fb0cb4cda991f
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2025-12-30 00:37:58 +0900

    deviseでビューページを作成

--- app/controllers/drafttop_controller.rb
+++ app/controllers/drafttop_controller.rb
@@ -1,4 +1,6 @@
 class DrafttopController < ApplicationController
+  # ログインしていない場合はログイン画面にリダイレクト
+  before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   def index
   end
