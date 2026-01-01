d6fe9e1be14d077fbd60673271d64bf8d0d80690
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-02 00:08:08 +0900

    update:新規登録時にユーザー名入力フォームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # アクション前に確認 devise関連画面でデバイス用のストロングパラメータを参照する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # devise用ストロングパラメータの設定 sign_up時にnameカラムキーを追加する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
