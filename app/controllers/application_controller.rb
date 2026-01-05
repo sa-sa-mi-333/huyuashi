335914775e1adc0928f9e29b8bddf1cfd57313a7
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-05 19:24:07 +0900

    add:ログイン後の条件分岐を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  # ログイン後の場合分け
+  def after_sign_in_path_for(resource)
+    if resource.user_status&.station_number.present?
+        authenticated_root_path
+    else
+        select_station_user_status_path
+    end
+  end
+
+
   # アクション前に確認 devise関連画面でデバイス用のストロングパラメータを参照する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
