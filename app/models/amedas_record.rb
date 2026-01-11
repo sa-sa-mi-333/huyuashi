556cfbcf019c1f0def0b4137c431cca4c20cfc61
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-11 16:52:19 +0900

    add:amedas_recordを追加

--- app/models/amedas_record.rb
+++ app/models/amedas_record.rb
@@ -1,3 +1,9 @@
 class AmedasRecord < ApplicationRecord
-    belongs_to :snow_station
+  belongs_to :snow_station,
+    foreign_key: :station_number,
+    primary_key: :station_number
+
+  # importメソッドの処理で必ず入力される
+  validates :station_number, presence: true
+  validates :json_date, presence: true
 end
