0b45eabe1ac0fb9aef9a93e07b542ea5faa2dcba
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-12 02:53:43 +0900

    update:最新のレコードが欠測の場合の処理を追加し、レコード情報をもとに[xx時現在]の記載を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,16 @@ class HomeController < ApplicationController
     # user_status.station_numberを元に観測地点の情報を表示する
     @station_number = current_user.user_status.station_number
     @station_info = SnowStation.find_by(station_number: @station_number)
+
+    # 気温と積雪の両方が有効な最新のレコードを取得
     @latest_record = AmedasRecord.where(station_number: @station_number)
+                                  .where.not(temp: nil)
+                                  .where.not(snow: nil)
                                   .order(json_date: :desc)
                                   .first
+    # もし両方が有効なレコードがなければ、最新のレコードを取得(ビュー側で欠測表示を行う)
+    @latest_record ||= AmedasRecord.where(station_number: @station_number)
+                                    .order(json_date: :desc)
+                                    .first
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,36 @@
 module ApplicationHelper
-    # deviseのresource_nameを定義する
-    def resource_name
-        :user
-    end
-    def resource
-        @resource ||= User.new
-    end
-    def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-    end
+  # deviseのresource_nameを定義する
+  def resource_name
+    :user
+  end
 
-    # ページタイトルを定義
-    def page_title(title = "")
-        base_title = "ふゆあし"
-        title.present? ? "#{title} | #{base_title}" : base_title
-    end
+  def resource
+    @resource ||= User.new
+  end
 
-    # ファイル名とオプションを指定してsvg画像をインライン要素として読み込む
-    def svg_tag(filename, options = {})
-      path = Rails.root.join("app/assets/images/icons", "#{filename}.svg")
-      if File.exist?(path)
-        File.read(path).html_safe
-      else
-        "<!-- SVG画像が読み込めませんでした -->".html_safe
-      end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  # ページタイトルを定義
+  def page_title(title = "")
+    base_title = "ふゆあし"
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
+
+  # ファイル名とオプションを指定してsvg画像をインライン要素として読み込む
+  def svg_tag(filename, options = {})
+    path = Rails.root.join("app/assets/images/icons", "#{filename}.svg")
+    if File.exist?(path)
+      File.read(path).html_safe
+    else
+      "<!-- SVG画像が読み込めませんでした -->".html_safe
     end
+  end
+
+  # xx時時点の時間を返す
+  def format_observation_hour(record)
+    return '---' if record.nil? || record.json_date.nil?
+    record.json_date.to_s[8..9]
+  end
 end
