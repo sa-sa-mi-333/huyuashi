0507d6d49ac5b9bba7ee75175b44cd6d54f79095
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-12 00:59:49 +0900

    fix:アメダス問い合わせ日時を格納するためjson_dateのデータ型をbigintに修正

---
+++ db/migrate/20260111154028_change_json_date_to_bigint_in_amedas_records.rb
@@ -0,0 +1,14 @@
+class ChangeJsonDateToBigintInAmedasRecords < ActiveRecord::Migration[8.0]
+  def up
+    # 既存のデータを削除(型変更のため)
+    AmedasRecord.delete_all
+
+    # json_dateカラムをbigintに変更(yyyymmddhh0000の形式で数値を保存するため)
+    change_column :amedas_records, :json_date, :bigint
+  end
+
+  def down
+    # ロールバック時の処理
+    change_column :amedas_records, :json_date, :integer
+  end
+end
