86107f25334dc14235c4325893ad30777bbfd6e6
sa_sa_mi_333 <sa_sa_mi_333@icloud.com>
2026-01-15 18:27:13 +0900

    user_statusモデルテストをfactorybotを使った内容に更新

--- spec/models/user_status_spec.rb
+++ spec/models/user_status_spec.rb
@@ -8,76 +8,50 @@ RSpec.describe UserStatus, type: :model do
 
     # belongs_to:user
     it 'user_idがあれば有効' do
-      user = User.create(email: 'valid_test@example.com', password: 'password')
-      user_status = user.user_status
-      expect(user_status).to be_valid
-      expect(user_status.errors).to be_empty
+      user = create(:user)
+      expect(user.user_status).to be_valid
+      expect(user.user_status.errors).to be_empty
     end
 
     it 'user_idがなければ無効' do
+      user_status = build(:user_status, user_id: '')
       expect {
-        UserStatus.create!
+        user_status.save!
       }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
-  # Userモデル作成時にUserStatusモデルをコールバックで作成
-  describe 'userモデル作成時にnameを受け取って設定する 未入力ならデフォルト名を設定' do
-    context 'デフォルト名が適用される場合' do
-      # name = blank
-      it 'デフォルト値「名無しの雪だるま」が設定される' do
-        user = User.create(email: 'blank_test@example.com', password: 'password', name: '')
-        expect(user.user_status.name).to eq('名無しの雪だるま')
-      end
-
-      # name = nil
-      it 'デフォルト値「名無しの雪だるま」が設定される' do
-        user = User.create(email: 'nil_test@example.com', password: 'password')
-        expect(user.user_status.name).to eq('名無しの雪だるま')
-      end
-    end
-
-    # name = xxx
-    context 'nameが指定されている場合' do
-      it '指定されたnameが設定される' do
-        user = User.create(email: 'add_name_test@example.com', password: 'password', name: '太郎')
-        expect(user.user_status.name).to eq('太郎')
-      end
-    end
-  end
+  # コールバックのnameチェックはuserモデル側で実施
 
   # default: 0, null: false
-  describe 'station_statusをenum{ inactive: 0, active: 1 }で設定' do
+  describe 'station_statusのenum設定' do
 
     context 'action_statusがnilの場合' do
       it '「inactive」が設定される' do
-        user = User.create(email: 'nilstatus_test@example.com', password: 'password', name: '太郎')
-        expect(user.user_status.action_status).to eq('inactive')
+        user_status = build(:user_status)
+        expect(user_status.action_status).to eq('inactive')
       end
     end
 
     context 'enumキーが設定されている場合' do
       it '「inactive」なら有効' do
-        user = User.create(email: 'inactive_test@example.com', password: 'password', name: '太郎')
-        expect(user.user_status.action_status).to eq('inactive')
+        user_status = build(:user_status, action_status: 'inactive')
+        expect(user_status.action_status).to eq('inactive')
       end
 
       it '「active」なら有効' do
-        user = User.create(email: 'active_test@example.com', password: 'password', name: '太郎')
-        user.user_status.action_status = 'active'
-        expect(user.user_status.action_status).to eq('active')
+        user_status = build(:user_status, action_status: 'active')
+        expect(user_status.action_status).to eq('active')
       end
     end
 
     context 'enumキー以外が設定されている場合' do
       it 'enumに設定されていない値は無効' do # enumの他の値にバリデーションを設定
-        user = User.create(email: 'other_test@example.com', password: 'password', name: '太郎')
-        user_status = UserStatus.create(user_id: user.id, action_status: :pending)
-        expect(user_status).to be_invalid
+        user_status = build(:user_status, action_status: 'pending')
+        expect {
+          user_status.save!
+        }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
-
-    # action_statusのparamは設定されていないのでblankの場合は考慮しない
-
   end
 end
